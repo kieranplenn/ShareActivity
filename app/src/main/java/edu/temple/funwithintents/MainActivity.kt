6dae8f5490537e697488b1b70d88fe21a883c05f
kieranplenn <tuk25850@temple.edu>
2024-11-07 12:24:56 -0500

    Idk why the commit won't show on github (nothings changed since due date, my Studio shows a commit).

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -12,9 +13,22 @@ class MainActivity : AppCompatActivity() {
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
+        val textToShare = editText.text.toString()
+
+        val sendIntent = Intent().apply{
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textToShare)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+
+            if (textToShare.isNotEmpty() && sendIntent.resolveActivity(packageManager) != null){
+                startActivity(shareIntent)
+            }
 
         }
     }
